2cff5bf58908b636a34ad614a6f59f4b5b3fe35e
PHM1605 <PHM1605@gmail.com>
2024-05-24 00:56:34 +0700

    updates

--- hellowin.cpp
+++ hellowin.cpp
@@ -1,19 +1,8 @@
-#include <iostream>
-#include <string>
-#pragma comment(lib, "winmm.lib")
 #include <Windows.h>
-#include <mmsystem.h>
 
-using namespace std;
-
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
-    cout << "Playing music\n";
-    PlaySound(TEXT("taunt.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
-    std::string input;
-    std::getline(std::cin, input);
-    PlaySound(0, 0, 0);
-    std::cout << "Stopped music\n";
+    
    
     return 0;
 }
